34a984c9999f92989b46b02d0cc3d21a0e21782a
Luka Mgaloblishvili <mgalobll@union.edu>
2022-01-22 15:02:25 -0500

    Updating Encoder class and adding Motor class

--- src/encoder.py
+++ src/encoder.py
@@ -8,19 +8,21 @@ class Encoder:
         self.encoder = rotaryio.IncrementalEncoder(pin_A, pin_B)
         self.prev_count = 0
 
-    def __repr__(self):
-        return str(self.encoder.position / 3)
+    
 
     def get_encoder_position(self):
         return self.encoder.position / 3
 
     def reset_encoder(self, pin_A, pin_B):
-        self.encoder = rotariyo.IncrementalEncoder(board.pin_A, board.pin_B)
+        self.encoder = rotaryio.IncrementalEncoder(board.pin_A, board.pin_B)
         self.prev_count = 0
 
-    def count(self):
-        cur_count = self.encoder.position / 3
-        dif = cur_count - self.prev_count
-        self.prev_count = cur_count
+    def __repr__(self):
+        return str(self.get_encoder_position())
 
-        return dif
+    # def count(self):
+    #     cur_count = self.encoder.position / 3
+    #     # dif = cur_count - self.prev_count
+    #     # self.prev_count = cur_count
+
+    #     return cur_count
